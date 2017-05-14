9f9de9a4efde68f5759040149e3e30e871750bc0
Rafael Almeida <ra_fa_94el@hotmail.com>
2017-05-15 00:25:16 +0100

    Initial Commit

---
+++ Sticky2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sticky2
+//
+//  Created by Rafael Almeida on 15/05/17.
+//  Copyright © 2017 Rafael Almeida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
